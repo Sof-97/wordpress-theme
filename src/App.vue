baba92ef68c52a9a82e01957828ba731e68422ec
Gerardo Calia <92599412+Sof-97@users.noreply.github.com>
2022-04-28 19:48:21 +0200

    first Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<header>
+      
+    </header>
+		<main></main>
+		<footer></footer>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "App",
+};
+</script>
+
+<style lang="scss">
+#app {
+}
+</style>
